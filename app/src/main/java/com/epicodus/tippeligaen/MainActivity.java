d7a8879a051041741625d9a528ffe0caec16cf89
Trevor <trevor.elvey@gmail.com>
2016-04-24 19:46:49 -0700

    create login activity

--- app/src/main/java/com/epicodus/tippeligaen/MainActivity.java
+++ app/src/main/java/com/epicodus/tippeligaen/MainActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.tippeligaen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButtonLogin;
+    private Button mButtonTable;
+    private Button mButtonFixtures;
+    private Button mButtonClubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonLogin = (Button) findViewById(R.id.buttonLogin);
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
