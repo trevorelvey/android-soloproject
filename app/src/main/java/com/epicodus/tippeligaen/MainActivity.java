aa57e8177dbeccefae6932bf9643bbf3c0957e5a
Trevor <trevor.elvey@gmail.com>
2016-04-24 19:47:06 -0700

    create table, fixture, and club activities

--- app/src/main/java/com/epicodus/tippeligaen/MainActivity.java
+++ app/src/main/java/com/epicodus/tippeligaen/MainActivity.java
@@ -6,22 +6,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mButtonLogin;
-    private Button mButtonTable;
-    private Button mButtonFixtures;
-    private Button mButtonClubs;
+    @Bind(R.id.buttonLogin) Button mButtonLogin;
+    @Bind(R.id.buttonTable) Button mButtonTable;
+    @Bind(R.id.buttonFixtures) Button mButtonFixtures;
+    @Bind(R.id.buttonClubs) Button mButtonClubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mButtonLogin = (Button) findViewById(R.id.buttonLogin);
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonFixtures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FixtureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonClubs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ClubActivity.class);
                 startActivity(intent);
             }
         });
