09dec8b164594e0570f1bedc95f6a72512c460a7
trevorelvey <trevor.elvey@gmail.com>
2016-04-28 12:01:05 -0700

    fix tippeligaen references

--- app/src/main/java/com/epicodus/eredivisie/adapters/ClubListAdapter.java
+++ app/src/main/java/com/epicodus/eredivisie/adapters/ClubListAdapter.java
@@ -1,5 +1,62 @@
 package com.epicodus.eredivisie.adapters;
 
-public class ClubListAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.epicodus.eredivisie.R;
+import com.epicodus.eredivisie.models.Club;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ClubListAdapter extends RecyclerView.Adapter<ClubListAdapter.ClubViewHolder> {
+    private ArrayList<Club> mClubs = new ArrayList<>();
+    private Context mContext;
+
+    public ClubListAdapter(Context context, ArrayList<Club> clubs) {
+        mContext = context;
+        mClubs = clubs;
+    }
+    @Override
+    public ClubListAdapter.ClubViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.club_list_item, parent, false);
+        ClubViewHolder viewHolder = new ClubViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ClubListAdapter.ClubViewHolder holder, int position) {
+        holder.bindClub(mClubs.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mClubs.size();
+    }
+
+    public class ClubViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.crestImageView)
+        ImageView mCrestImageView;
+        @Bind(R.id.clubNameTextView)
+        TextView mClubNameTextView;
+        private Context mContext;
+
+
+        public ClubViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindClub(Club club) {
+            mClubNameTextView.setText(club.getName());
+        }
+    }
 }
--- app/src/main/java/com/epicodus/eredivisie/ui/ClubActivity.java
+++ app/src/main/java/com/epicodus/eredivisie/ui/ClubActivity.java
@@ -3,21 +3,22 @@ package com.epicodus.eredivisie.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epicodus.eredivisie.R;
+import com.epicodus.eredivisie.adapters.ClubListAdapter;
 import com.epicodus.eredivisie.models.Club;
 import com.epicodus.eredivisie.services.ApiService;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.RunnableFuture;
 
 
 import butterknife.Bind;
@@ -27,9 +28,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class ClubActivity extends AppCompatActivity {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private ClubListAdapter mAdapter;
     public ArrayList<Club> mClubs = new ArrayList<>();
     public static final String TAG = ClubActivity.class.getSimpleName();
-    @Bind(R.id.recyclerView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +39,8 @@ public class ClubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_club);
         ButterKnife.bind(this);
 
-        mListView = (ListView) findViewById(R.id.recyclerView);
-
         Intent intent = getIntent();
+
         getClubs();
     }
     private void getClubs() {
@@ -58,19 +59,12 @@ public class ClubActivity extends AppCompatActivity {
                 ClubActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        String[] clubNames = new String[mClubs.size()];
-                        for (int i = 0; i < clubNames.length; i++) {
-                            clubNames[i] = mClubs.get(i).getName();
-                        }
-                        ArrayAdapter adapter = new ArrayAdapter(ClubActivity.this,
-                                android.R.layout.simple_list_item_1, clubNames);
-                        mListView.setAdapter(adapter);
-
-                        for (Club club : mClubs) {
-                            Log.d(TAG, club.getName());
-                            Log.d(TAG, club.getCrest());
-                        }
+                        mAdapter = new ClubListAdapter(getApplicationContext(), mClubs);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                        new LinearLayoutManager(ClubActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
                     }
                 });
             }
