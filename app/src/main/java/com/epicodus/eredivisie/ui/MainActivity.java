2f0f8358f0d373261fc25b50b0ec05ca3e709bbe
trevorelvey <trevor.elvey@gmail.com>
2016-05-18 14:45:21 -0700

    add drawer, displays but doesn't link yet

--- app/src/main/java/com/epicodus/eredivisie/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/eredivisie/ui/MainActivity.java
@@ -2,8 +2,12 @@ package com.epicodus.eredivisie.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,8 +17,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.eredivisie.Constants;
 import com.epicodus.eredivisie.R;
@@ -24,21 +32,28 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @Bind(R.id.buttonTable) Button mButtonTable;
-    @Bind(R.id.buttonFixtures) Button mButtonFixtures;
-    @Bind(R.id.buttonClubs) Button mButtonClubs;
-    @Bind(R.id.websiteButton) Button mWebsiteButton;
-    @Bind(R.id.welcomeTextView) TextView mWelcomeTextView;
-    @Bind(R.id.savedFixtureButton) Button mSavedFixtureButton;
+public class MainActivity extends ActionBarActivity {
+//    @Bind(R.id.buttonTable) Button mButtonTable;
+//    @Bind(R.id.buttonFixtures) Button mButtonFixtures;
+//    @Bind(R.id.buttonClubs) Button mButtonClubs;
+//    @Bind(R.id.websiteButton) Button mWebsiteButton;
+//    @Bind(R.id.welcomeTextView) TextView mWelcomeTextView;
+//    @Bind(R.id.savedFixtureButton) Button mSavedFixtureButton;
     private ValueEventListener mUserRefListener;
     private Firebase mUserRef;
     private SharedPreferences mSharedPreferences;
     private Firebase mFirebaseRef;
     private String mUId;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,31 +62,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
         mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
 
-        mButtonTable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TableActivity.class);
-                startActivity(intent);
-            }
-        });
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
 
-        mButtonFixtures.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FixtureActivity.class);
-                startActivity(intent);
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
-        mButtonClubs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClubActivity.class);
-                startActivity(intent);
-            }
-        });
+        addDrawerItems();
+        setupDrawer();
 
-        mWebsiteButton.setOnClickListener(this);
+//        mButtonTable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, TableActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        mButtonFixtures.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, FixtureActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        mButtonClubs.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ClubActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        mWebsiteButton.setOnClickListener(this);
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mUId = mSharedPreferences.getString(Constants.KEY_UID, null);
@@ -81,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
-                mWelcomeTextView.setText("Hi, " + user.getUsername() + "!");
+//                mWelcomeTextView.setText("Hi, " + user.getUsername() + "!");
             }
 
             @Override
@@ -89,21 +114,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        mSavedFixtureButton.setOnClickListener(this);
+//        mSavedFixtureButton.setOnClickListener(this);
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "League Table", "Fixtures", "Clubs", "My Fixtures", "Official Website"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return true;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_logout) {
-            logout();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -121,16 +202,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == mWebsiteButton) {
-            Intent webIntent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("http://www.eredivisie.nl/"));
-            startActivity(webIntent);
-        }
-        if (v == mSavedFixtureButton) {
-            Intent intent = new Intent(MainActivity.this, SavedFixtureListActivity.class);
-            startActivity(intent);
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        if (v == mWebsiteButton) {
+//            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+//                    Uri.parse("http://www.eredivisie.nl/"));
+//            startActivity(webIntent);
+//        }
+//        if (v == mSavedFixtureButton) {
+//            Intent intent = new Intent(MainActivity.this, SavedFixtureListActivity.class);
+//            startActivity(intent);
+//        }
+//    }
 }
